002cb87c0acdb6ee3f3743f11d2e66d6c0374e75
Leung Wai Liu <leungwai@wustl.edu>
2022-03-04 19:23:05 -0600

    Working the app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
 # configuration
@@ -6,11 +7,15 @@ DEBUG = True
 
 # instantiate the app
 app = Flask(__name__)
-app.config.from_object(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://vaviwhocxsnomz:c274c35a9cb7d6c8bd8418ffbcda4431d407ffafca6d5e59ef2018e37aec61ef@ec2-34-230-110-100.compute-1.amazonaws.com:5432/d3ug8s7afnkd2o'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_size': 100, 'pool_recycle': 280}
 
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+class ProductivityApp(db.Model):
+    
 
 # sanity check route
 @app.route('/ping', methods=['GET'])
